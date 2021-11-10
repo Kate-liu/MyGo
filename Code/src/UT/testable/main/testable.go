5b0fa9f2bffce658ccc45e499cba25029e61e8a9
rmliu <2218207429@qq.com>
2021-11-11 01:30:12 +0800

    feat(feat): add new code files.

---
+++ Code/src/UT/testable/main/testable.go
@@ -0,0 +1,13 @@
+package main
+
+type Post struct {
+	Name    string
+	Address string
+}
+type Service interface {
+	ListPosts() ([]*Post, error)
+}
+
+func ListPosts(svc Service) ([]*Post, error) {
+	return svc.ListPosts()
+}
---
+++ Code/src/UT/untestable/main/untestable.go
@@ -0,0 +1,12 @@
+package main
+
+import "google.golang.org/grpc"
+
+type Post struct {
+	Name    string
+	Address string
+}
+
+func ListPosts(client *grpc.ClientConn) ([]*Post, error) {
+	return client.ListPosts()
+}
