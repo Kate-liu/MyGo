e00dd0eca16e7686590e03acbfe0be1e99443e5f
rmliu <2218207429@qq.com>
2021-11-17 22:50:48 +0800

    feat(feat): add hello world cmd code files.

---
+++ Code/src/TrashFile/main/cmd.go
@@ -0,0 +1,13 @@
+package main
+
+//import (
+//	"github.com/marmotedu/iam/internal/iamctl/cmd/helloworld"
+//)
+//	...
+//	{
+//		Message: "Troubleshooting and Debugging Commands:",
+//		Commands: []*cobra.Command{
+//			validate.NewCmdValidate(f, ioStreams),
+//			helloworld.NewCmdHelloworld(f, ioStreams),
+//		},
+//	}
